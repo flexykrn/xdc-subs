6f5bc3d2b43dcc87f6f21e8c260ca68b96e90128
flexykrn <knpawar6000@gmail.com>
2026-04-28 18:17:02 +0530

    fix: wrap Web3Auth provider for Etherspot compatibility

--- subscription-manager/app/src/lib/etherspot.ts
+++ subscription-manager/app/src/lib/etherspot.ts
@@ -17,7 +17,35 @@ export async function getEtherspotPrime(web3Provider: any): Promise<PrimeSdk> {
   const apiKey = url.searchParams.get("api-key") || undefined;
   const baseUrl = `${url.protocol}//${url.host}${url.pathname}`;
 
-  const primeSdk = new PrimeSdk(web3Provider, {
+  // Wrap Web3Auth provider to match Etherspot's EthereumProvider interface
+  const wrappedProvider = {
+    request: async (args: { method: string; params?: unknown[] | object }) => {
+      return web3Provider.request(args);
+    },
+    sendAsync: (args: any, callback: any) => {
+      web3Provider.request(args).then((result: any) => callback(null, { jsonrpc: "2.0", id: args.id, result })).catch((err: any) => callback(err));
+    },
+    on: (event: string, callback: any) => {
+      if (web3Provider.on) web3Provider.on(event, callback);
+    },
+    once: (event: string, callback: any) => {
+      if (web3Provider.once) web3Provider.once(event, callback);
+    },
+    removeListener: (event: string, callback: any) => {
+      if (web3Provider.removeListener) web3Provider.removeListener(event, callback);
+    },
+    off: (event: string, callback: any) => {
+      if (web3Provider.off) web3Provider.off(event, callback);
+    },
+    disconnect: async () => {
+      if (web3Provider.disconnect) await web3Provider.disconnect();
+    },
+    accounts: [],
+    chainId: chainId,
+    isWalletConnect: false,
+  };
+
+  const primeSdk = new PrimeSdk(wrappedProvider as any, {
     chainId,
     bundlerProvider: new EtherspotBundler(chainId, apiKey, baseUrl),
   });
